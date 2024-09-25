9a3aed5fa906b02659f61d97b49b3955cace0409
Marwan <smarouan@gmail.com>
2024-09-25 22:31:12 +0100

    refactoring processor

---
+++ src/main/java/com/example/testkatatondeusemarouan/TestKataTondeuseMarouanApplication.java
@@ -0,0 +1,13 @@
+package com.example.testkatatondeusemarouan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestKataTondeuseMarouanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestKataTondeuseMarouanApplication.class, args);
+    }
+
+}
